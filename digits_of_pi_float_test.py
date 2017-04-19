26388d730f6af67607697cd7c8665f1e9883e060
n-Burn <7nburn@gmail.com>
2017-04-18 23:08:20 -0400

    int conversion not necessary

--- digits_of_pi_float_test.py
+++ digits_of_pi_float_test.py
@@ -1,3 +1,5 @@
+# tested with Blender 2.78c
+
 import bpy
 from mathutils import Vector
 
@@ -6,4 +8,4 @@ pi42 = "314159265358979323846264338327950288419716"
 for i in range( 0, len(pi42) ):
     piFlt = float( pi42[:i] + '.' + pi42[i:] )
     piVec = Vector(( i, piFlt ))
-    print( '%2d' % int(piVec[0]), piVec[1] )
+    print( '%2d' % piVec[0], piVec[1] )
