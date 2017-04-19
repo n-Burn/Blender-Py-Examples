5c8331157270f95636b77245e8a55c5b522c9801
n-Burn <7nburn@gmail.com>
2017-04-18 22:52:03 -0400

    tabs to spaces

--- digits_of_pi_float_test.py
+++ digits_of_pi_float_test.py
@@ -2,8 +2,8 @@ import bpy
 from mathutils import Vector
 
 print()
-x = "314159265358979323846264338327950288419716"
-for i in range( 0, len(x) ):
-	y = float( x[:i] + '.' + x[i:] )
-	z = Vector(( i, y ))
-	print( '%2d' % int(z[0]), z[1] )
+pi42 = "314159265358979323846264338327950288419716"
+for i in range( 0, len(pi42) ):
+    piFlt = float( pi42[:i] + '.' + pi42[i:] )
+    piVec = Vector(( i, piFlt ))
+    print( '%2d' % int(piVec[0]), piVec[1] )
