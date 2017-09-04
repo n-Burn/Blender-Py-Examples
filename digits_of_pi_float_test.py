8a50a6ec1152302e4063a4d0a9d14a761b213a6a
n-Burn <7nburn@gmail.com>
2017-09-04 16:36:34 -0400

    pep8 cleanup

--- digits_of_pi_float_test.py
+++ digits_of_pi_float_test.py
@@ -5,7 +5,7 @@ from mathutils import Vector
 
 print()
 pi42 = "314159265358979323846264338327950288419716"
-for i in range( 0, len(pi42) ):
-    piFlt = float( pi42[:i] + '.' + pi42[i:] )
-    piVec = Vector(( i, piFlt ))
-    print( '%2d' % piVec[0], piVec[1] )
+for i in range(0, len(pi42)):
+    pi_flt = float(pi42[:i] + '.' + pi42[i:])
+    pi_vec = Vector((i, pi_flt))
+    print('%2d' % pi_vec[0], pi_vec[1])
