bfd6a0c5b1678abf9578fa541d33d32ecfd612ca
n-Burn <7nburn@gmail.com>
2019-07-29 15:48:01 -0400

    code cleanup

--- digits_of_pi_float_test.py
+++ digits_of_pi_float_test.py
@@ -1,4 +1,4 @@
-# tested with Blender 2.78c
+# tested with Blender 2.78, 2.79, 2.80
 
 import bpy
 from mathutils import Vector
@@ -6,8 +6,8 @@ from mathutils import Vector
 print()
 pi42 = "314159265358979323846264338327950288419716"
 print(pi42, '\n')
+pi_vec = Vector()
 for i in range(0, len(pi42)):
-    pi_flt = float(pi42[:i] + '.' + pi42[i:])
-    pi_vec = Vector((i, pi_flt))
-    print('%2d' % pi_vec[0], pi_vec[1])
+    pi_vec[0] = float(pi42[:i] + '.' + pi42[i:])
+    print('%2d' % i, pi_vec[0])
 print()
