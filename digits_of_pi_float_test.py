0b5e3dcbaddf99ce186dddcad20925e45c112e41
n-Burn <7nburn@gmail.com>
2019-07-29 15:22:47 -0400

    added pi digits to print out

--- digits_of_pi_float_test.py
+++ digits_of_pi_float_test.py
@@ -5,7 +5,9 @@ from mathutils import Vector
 
 print()
 pi42 = "314159265358979323846264338327950288419716"
+print(pi42, '\n')
 for i in range(0, len(pi42)):
     pi_flt = float(pi42[:i] + '.' + pi42[i:])
     pi_vec = Vector((i, pi_flt))
     print('%2d' % pi_vec[0], pi_vec[1])
+print()
